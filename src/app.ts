0a828c4dbbfc1a9c0e8c2770a1e774ab192275c4
arustamdev <arustamdev@gmail.com>
2025-04-22 21:12:06 +0200

    dotenv, morgan, helmet added; structure changed

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.json());
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import express from 'express';
+import app from './app';
+import 'dotenv/config';
 
-const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (_req, res) => {
-    res.send('Hello from TypeScript + Express!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
