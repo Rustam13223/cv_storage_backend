21dc29fae372d87c86ed135596ec03dfc69058bf
arustamdev <arustamdev@gmail.com>
2025-04-24 08:32:30 +0200

    authenticate middleware added

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 
 import authRouter from '@/routes/auth';
+import { authenticate } from './middleware/auth';
 
 const app = express();
 
@@ -11,5 +12,9 @@ app.use(helmet());
 app.use(express.json());
 
 app.use('/', authRouter);
+
+app.get('/protected', authenticate, (req, res) => {
+    res.status(200).json({ message: 'This is a protected route' });
+});
 
 export default app;
---
+++ src/middleware/auth/index.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+
+import jwt from 'jsonwebtoken';
+
+interface JwtPayload {
+    email: string;
+}
+
+export interface AuthenticatedRequest extends Request {
+    user?: JwtPayload;
+}
+
+export const authenticate = async function (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+): Promise<void> {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        res.status(401).json({ message: 'No token provided' });
+        return;
+    }
+
+    try {
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET as string
+        ) as JwtPayload;
+        req.user = decoded;
+        next();
+        // eslint-disable-next-line
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
