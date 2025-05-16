679052028b6ea9752a22cb3db0bc251ec7f405d3
arustamdev <arustamdev@gmail.com>
2025-05-16 15:21:57 +0200

    projects delete and put routes added

--- src/controllers/projects/index.ts
+++ src/controllers/projects/index.ts
@@ -2,8 +2,22 @@ import { Response } from 'express';
 import { AuthenticatedRequest } from '@/middleware/auth';
 import { projects } from '@/db/schema/projects';
 import { db } from '@/db';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 import { JwtPayload } from '@/middleware/auth';
+
+export const deleteById = async function (
+    req: AuthenticatedRequest,
+    res: Response
+): Promise<void> {
+    const projectId = Number(req.params.id);
+
+    const deletedProject = await db
+        .delete(projects)
+        .where(eq(projects.id, projectId))
+        .returning();
+
+    res.status(200).json(deletedProject);
+};
 
 export const getAll = async function (
     req: AuthenticatedRequest,
@@ -55,4 +69,40 @@ export const create = async function (
         .returning();
 
     res.status(201).json(project);
+};
+
+export const update = async function (
+    req: AuthenticatedRequest,
+    res: Response
+): Promise<void> {
+    const user = req.user as JwtPayload;
+
+    const { title, description } = req.body;
+    const projectId = Number(req.params.id);
+
+    if (!title || !description) {
+        res.status(400);
+        return;
+    }
+
+    const candidate = await db.query.projects.findFirst({
+        where: (fields, { eq, and }) =>
+            and(eq(fields.id, projectId), eq(fields.userId, user.userId)),
+    });
+
+    if (!candidate) {
+        res.status(400);
+        return;
+    }
+
+    const updatedProject = await db
+        .update(projects)
+        .set({
+            title,
+            description,
+        })
+        .where(eq(projects.id, projectId))
+        .returning();
+
+    res.status(200).json(updatedProject);
 };
--- src/routes/projects/index.ts
+++ src/routes/projects/index.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
-import { create, getAll, getById } from '@/controllers/projects';
+import {
+    create,
+    getAll,
+    getById,
+    deleteById,
+    update,
+} from '@/controllers/projects';
 
 const projectsRouter = Router();
 
 projectsRouter.get('/projects', getAll);
 projectsRouter.get('/projects/:id', getById);
 projectsRouter.post('/projects', create);
+projectsRouter.delete('/projects/:id', deleteById);
+projectsRouter.put('/projects/:id', update);
 
 export default projectsRouter;
